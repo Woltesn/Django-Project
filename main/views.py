b5f991e0d5f6ee354872e8169ca2a4ce01c08ce6
Dmitriy Petrenko <dmitriy.petrenko@anvileight.com>
2021-08-04 14:55:56 +0300

    Create new project

---
+++ main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.order_by('title')[:10]
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
